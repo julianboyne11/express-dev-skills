9e3ce9a7eb3896a3de19dc302bbd3757bc77327b
julianboyne <julianboyne11@gmail.com>
2022-09-20 19:28:19 -0400

    skills-lab

---
+++ data/skills-data.js
@@ -0,0 +1,9 @@
+const skills = [
+  {text: 'Comunication', have: true, _id: 125223},
+  {text: 'Interpersonal', have: false, _id: 127904},
+  {text: 'Leadership', have: false, _id: 139608},
+]
+
+export { 
+	skills
+}
